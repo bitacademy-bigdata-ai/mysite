b5d8920904005d73c16cb78a9afc46591870443e
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-11-11 09:58:53 +0900

    encoding filter

--- mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -10,8 +10,6 @@ public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		
 		request
 			.getRequestDispatcher("/WEB-INF/views/main/index.jsp")
 			.forward(request, response);
